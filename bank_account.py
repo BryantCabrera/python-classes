73f6471f74204fa2036ac68157065537c6bf51a9
Bryant <cabrera.bryant@gmail.com>
2019-02-20 14:13:08 -0800

    Created overdraft fees property on BankAccount class.

--- bank_account.py
+++ bank_account.py
@@ -1,10 +1,11 @@
 class BankAccount():
     totalBalance = 0
 
-    def __init__(self, name="", account_type="", balance=0):
-        self.name = name
+    def __init__(self, account_type="", name="", balance=0, overdraft_fees=0):
         self.account_type = account_type
+        self.name = name
         self.balance = balance
+        self.overdraft_fees = overdraft_fees
 
     def deposit(self, amount):
         self.balance += amount
@@ -15,11 +16,24 @@ class BankAccount():
     def withdraw(self, amount):
         self.balance -= amount
         print(f"{self.name}'s balance just dropped to {self.balance}.")
+
+        if self.balance < 0:
+            self.overdraft_fees += 20
+            print(f"{self.name}'s balance just dropped below 0 and {self.name} was charged $20.  You now have ${self.overdraft_fees} in overdraft fees.")
+
         BankAccount.totalBalance -= amount
         print(f"Total balance just dropped to {BankAccount.totalBalance}.")
 
 
-bryant = BankAccount('Bryant', 'savings', 4000000)
+# class ChildBankAccount(BankAccount):
+#     def __init__(self, account_type="", ):
+#         BankAccount.__init__(self)
+#         print('New account made:', self)
+
+
+
+bryant = BankAccount('savings', 'Bryant', 4000000)
 print(bryant.balance)
 
 bryant.deposit(1000000)
+bryant.withdraw(6000000)
